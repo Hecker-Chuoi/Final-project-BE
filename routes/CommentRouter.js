c7cd18a2edfea593454460e65332d9561f797807
Hecker - Chuối <nguyentienht1910@gmail.com>
2025-06-04 10:56:54 +0700

    init

---
+++ routes/CommentRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const comment = require("../db/commentModel");
+const router = express.Router();
+
+router.post("/", async (request, response) => {});
+
+router.get("/", async (request, response) => {});
+
+module.exports = router;
